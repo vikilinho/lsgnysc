fa7a064b3447f72091ee29e02fac534acfd9108e
victor <dvicxy@gmail.com>
2022-01-19 17:06:11 +0100

    new addon

--- lib/pages/bvn_screen.dart
+++ lib/pages/bvn_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lsgnysc/pages/navigation_screen.dart';
 
 class BvnScreen extends StatefulWidget {
   const BvnScreen({Key? key}) : super(key: key);
@@ -12,24 +13,21 @@ class _BvnScreenState extends State<BvnScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Complete Registration"),
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text(
-                "Federal Ministry of Education",
-                style: GoogleFonts.mulish(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
+            SizedBox(
+              height: 130,
             ),
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: Text(
-                "Fill your BVN Details to complete your registration",
+                "Fill your BVN Details to complete your registration.",
                 style: GoogleFonts.mulish(
                   fontSize: 20,
                   fontWeight: FontWeight.w600,
@@ -48,6 +46,42 @@ class _BvnScreenState extends State<BvnScreen> {
                 hintStyle: GoogleFonts.mulish(),
                 labelText: "12345678912",
               )),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: ButtonTheme(
+                minWidth: 300,
+                height: 50,
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                    color: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    onPressed: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => NavigationScreen()),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Complete Registration",
+                          style: GoogleFonts.mulish(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Icon(Icons.arrow_forward, color: Colors.white),
+                      ],
+                    )),
+              ),
             ),
           ],
         ),
