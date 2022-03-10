c9b48ddce6dc4bd826fae4ac58277ab290464896
victor <dvicxy@gmail.com>
2022-03-10 14:31:04 +0100

    kyc form

--- lib/pages/polaris_form.dart
+++ lib/pages/polaris_form.dart
@@ -20,6 +20,10 @@ class _PolarisFormState extends State<PolarisForm> {
           SizedBox(
             height: 20,
           ),
+          Text("Do you have an account with Polaris bank?"),
+          SizedBox(
+            height: 5,
+          ),
           Row(
             children: [
               Expanded(
@@ -31,8 +35,7 @@ class _PolarisFormState extends State<PolarisForm> {
                       decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           alignLabelWithHint: true,
-                          hintText: "Sex",
-                          label: Text("Gender")),
+                          label: Text("Yes/No")),
                     ),
                   ),
                 ),
@@ -49,8 +52,63 @@ class _PolarisFormState extends State<PolarisForm> {
                       decoration: InputDecoration(
                           border: OutlineInputBorder(),
                           alignLabelWithHint: true,
-                          hintText: "Sex",
-                          label: Text("Gender")),
+                          label: Text("Branch")),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.transparent),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          alignLabelWithHint: true,
+                          label: Text("Account N0")),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.transparent),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          alignLabelWithHint: true,
+                          label: Text("BVN")),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.transparent),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          alignLabelWithHint: true,
+                          hintText: "Bank Name",
+                          label: Text("Other Bank Account")),
                     ),
                   ),
                 ),
