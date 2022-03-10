958bef900f6ca481120fbb52980cddfb52aef494
victor <dvicxy@gmail.com>
2022-03-10 14:14:48 +0100

    kyc form

---
+++ lib/pages/polaris_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PolarisForm extends StatefulWidget {
+  const PolarisForm({Key? key}) : super(key: key);
+
+  @override
+  State<PolarisForm> createState() => _PolarisFormState();
+}
+
+class _PolarisFormState extends State<PolarisForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
